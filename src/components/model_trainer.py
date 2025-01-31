d510d23fb69c509221a6d058367a284c135b41da
chrisdcruz1 <christopherdcruzcoc@gmail.com>
2025-01-31 22:12:50 +0530

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -90,6 +90,7 @@ class ModelTrainer:
 
             ''' To get best model score from dict '''
             best_model_score = max(sorted(model_report.values()))
+            print(best_model_score)
 
             ''' To get best model name from dict '''
 
@@ -97,6 +98,7 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+            print(best_model_name)
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
